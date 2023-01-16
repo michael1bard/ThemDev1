1a48356395448aacae67748fee44a730f956e310
halcolm1 <michael1bard@gmail.com>
2023-01-15 23:25:33 -0500

    added sorting/filters

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,10 @@
 // Put your application javascript here
 // alert("i have an alter box");
+if (document.getElementById("sort_by") != null) {
+  document.querySelector("#sort_by").addEventListener("change", function (e) {
+    const url = new URL(window.location.href);
+    url.searchParams.set("sort_by", e.currentTarget.value);
+
+    window.location = url.href;
+  });
+}
