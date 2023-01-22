5f34d1130f3c8482b6f8d7032afc75cd45f78eee
halcolm1 <michael1bard@gmail.com>
2023-01-21 22:58:11 -0500

    update address page

--- assets/application.js
+++ assets/application.js
@@ -8,3 +8,33 @@ if (document.getElementById("sort_by") != null) {
     window.location = url.href;
   });
 }
+
+// dropdown box in addresses page
+if (document.getElementById("AddressCountryNew") != null) {
+  document
+    .getElementById("AddressCountryNew")
+    .addEventListener("change", function (e) {
+      const provinces =
+        this.options[this.selectedIndex].getAttribute("data-province");
+      const provinceSelector = document.getElementById("AddressProvinceNew");
+      const provinceArray = JSON.parse(provinces);
+    });
+
+  if (provinceArray.length < 1) {
+    provinceSelector.setAttribute("disabled", "disabled");
+  } else {
+    provinceSelector.removeAttribute("disabled");
+  }
+
+  provinceSelector.innerHTML = "";
+  let options = "";
+  for (let i = 0; i < provinceArray.length; i++) {
+    options +=
+      '<option value ="' +
+      provinceArray[i][0] +
+      '">' +
+      provinceArray[i][0] +
+      "</option>";
+  }
+  provinceSelector.innerHTML = options;
+}
